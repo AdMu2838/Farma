4683b3a30db3c3e3eb35592d8edfefeddd3ad449
Agustin Mamani <agusdmu1999@gmail.com>
2022-12-07 09:15:36 -0500

    conexion a la base de datos

---
+++ src/util/MySQLConnection.java
@@ -0,0 +1,27 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQLConnection {
+	public static Connection getConnection() {
+		Connection cn = null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			String url = "jdbc:mysql://localhost:3306/Farmacia?useSSL=false&useTimezone=true&serverTimezone=UTC";
+			String user = "root";
+			String pass = "Dextro2838-";
+			cn = DriverManager.getConnection(url, user, pass);
+		} catch (ClassNotFoundException e) {
+			System.out.println("Error >> Driver no Instalado!!" + e.getMessage());
+		} catch (SQLException e) {
+			System.out.println("Error >> de conexi�n con la BD" + e.getMessage());
+		} catch (Exception e) {
+			System.out.println("Error >> general : " + e.getMessage());
+		} 
+		
+		return cn;
+	}
+}
